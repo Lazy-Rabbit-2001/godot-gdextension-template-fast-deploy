900e90689dfd2164fcffe07fcfd6fa4b413329df
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-06-30 12:26:38 +0800

    updated deploy.bat

--- DEPLOY.bat
+++ DEPLOY.bat
@@ -2,14 +2,14 @@
 
 echo Starting deployment of renaming tool...
 cd .\deployment\
-python .\deploy
+python .\deploy.py
 echo Done deployment.
 
 timeout /t 3 /nobreak >nul
 
 echo Starting deployment of environment...
 cd ..\godot-cpp\
-scons platform=windows custom_api_file=custom_api.json
+scons platform=windows custom_api_file=extension_api.json
 echo Done deployment of environment...
 
 timeout /t 3 /nobreak >nul
