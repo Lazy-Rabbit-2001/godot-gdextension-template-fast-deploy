4ce60bda7b3e43eb7c6a3f474d6239dad3f9c144
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-06-30 12:32:05 +0800

    fixed some bugs

--- DEPLOY.bat
+++ DEPLOY.bat
@@ -1,14 +1,13 @@
 @echo off
 
-echo Starting deployment of renaming tool...
-cd .\deployment\
+echo Starting deployment of renaming...
 python .\deploy.py
-echo Done deployment.
+echo Done deployment of renaming..
 
 timeout /t 3 /nobreak >nul
 
 echo Starting deployment of environment...
-cd ..\godot-cpp\
+cd .\godot-cpp\
 scons platform=windows custom_api_file=extension_api.json
 echo Done deployment of environment...
 
